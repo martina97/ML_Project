0a7745e3f7de54a3068e957fa9bba9f002551033
ceciliacal <c.calavaro@yahoo.it>
2021-06-23 00:08:44 +0200

    benchmark a 0.99699

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from pandas import read_csv
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler, RobustScaler
 
-scale_type = "STANDARD"
+scale_type = "MAX_ABS "
 
 
 def load_file(filepath):
@@ -26,15 +26,15 @@ def load_file(filepath):
     return dataframe.values
 
 
-# load a list of files and return as a 3d numpy array
-# We can then load all data for a given group (train or test) into a single three-dimensional NumPy array,
-# where the dimensions of the array are [samples, time steps, features].
+# Carica una lista di files e restituisce un array 3D
+# con dimensione è [samples, timesteps, features] (cioè [5000,315,3])
+
 def load_group(filenames):
     loaded = list()
     for name in filenames:
         data = load_file(name)
         loaded.append(data)
-    # stack group so that features are the 3rd dimension
+    # feature in dimensione 3rd
     loaded = dstack(loaded)
     return loaded
 
@@ -43,7 +43,6 @@ def replaceCommaCsv(filenames):
     filenames2 = list()
     for name in filenames:
         data = ""
-
         with open(name + '.csv') as file:
             data = file.read().replace(",", " ")
 
@@ -53,7 +52,7 @@ def replaceCommaCsv(filenames):
         filenames2 += [name + '.txt']
     return filenames2
 
-
+#trasformazione formato files da csv a txt
 def csvToTxt(path):
     text = open(path + ".csv", "r")
     text = ''.join([i for i in text]) \
@@ -62,67 +61,83 @@ def csvToTxt(path):
     x.writelines(text)
     x.close()
 
-
+#caricamento dei dati
 def loadData():
+    csvToTxt('train_gesture_x')
+    csvToTxt('train_gesture_y')
+    csvToTxt('train_gesture_z')
+    csvToTxt('train_label')
     # load data
     filenames = list()
     filenames += ['train_gesture_x.txt', 'train_gesture_y.txt', 'train_gesture_z.txt']
     trainX = load_group(filenames)
-
-    csvToTxt('train_label')
     trainY = load_file('train_label.txt')
-
     return trainX, trainY
 
-
-def getNaCount(dataset):
+def getNaCount2(filePath):
+    dataset = pd.read_csv(filePath, header= None)
     # per ogni elemento (i,j) del dataset, isna() restituisce
     # TRUE/FALSE se il valore corrispondente è mancante/presente
+    '''
+    print("\n ----train", dataset.values)
+    print("shape= ", dataset.shape)
+    print("PROVA NAN = " ,dataset.values[0][314])
+    '''
 
-    print("\n ----train[0] ", dataset[0])
-    print("shape= ", dataset.shape[0])
+    df = pd.DataFrame(dataset.values)
+    boolean_mask = df.isna()
+    '''
+    print("----mask: ", boolean_mask)
+    print("SHAPE MASK ========= " , boolean_mask.shape)
+    print(" PROVAAAA ========= " , boolean_mask[1])
+    '''
+    #la boolean mask viene scorsa per colonne
+    for col in range(0,  boolean_mask.shape[1]) :   #colonne
+        #print("col ===" , col)
+        for row in range(0, boolean_mask.shape[0]): #righe
+            #print("row ===", row)
+            if boolean_mask[col][row] == True:
+                #print("cecilia")
+                #print("NAN == " , dataset.values[row][col])
+                # se i = 0 --> prendi i+1
+                # se i != 0 = 315 ???? --> prendi i-1
+                if col == 0:
+                    print(dataset.values[row][col+1])
+                    substitutionValue = dataset.values[row][col+1]  #valore successivo nella riga j che metto al posto del nan
+                    #dataset.values[row][col] = substitutionValue
+                    dataset.at[row,col] = substitutionValue
+                    #print("MERDA ==" , dataset.value[0][1])
+                else :
+                    #print("la colonna di merda e' -------", col)
+                    #print(dataset.values[row][col - 1])
+                    substitutionValue = dataset.values[row][col - 1]  # valore precedente nella riga j che metto al posto del nan
+                    #dataset.values[row][col] = substitutionValue
+                    #print("cacca ===",dataset.values[row][col] )
+                    dataset.at[row, col] = substitutionValue
 
-    # print("x_train: ", x_train)
-
-    for i in range(0, dataset.shape[0]):
-        print("i= ", i)
-
-        bho = pd.DataFrame(dataset[i])
-        # print("\n---bho[0] ", bho)
-        boolean_mask = bho.isna()
-        # contiamo il numero di TRUE per ogni attributo sul dataset
-        '''
-                if (boolean_mask == False):
-            print("MERDA")
-
-        '''
-
-        print("----mask: ", boolean_mask)
-        count = boolean_mask.sum(axis=0)
-
-        print("count NaN: ", count)
-        print("count iloc sto cazzo: ", count.values)
-
-        stronzo = 1 in count.values
-        print("stronzo = ", stronzo)
+    dfAfterReplacement = pd.DataFrame(dataset.values)
+    '''
+    print("LIGLI CHE PALLE AIUTO ----------------------------------------------" , dataset.values[0][1])
+    print("LIGLI CHE PALLE AIUTO ----------------------------------------------" , dataset.values[0][314])
+    print("LIGLI CHE PALLE AIUTO ----------------------------------------------" , dataset.values[0][0])
+    '''
+    dfAfterReplacement.to_csv("provaNan.csv",  header= None, index = None)
 
 
 # Expanding window
 def kFoldValidation(trainX, trainY, testX, testY):
     print(" -------------  kFoldValidation   ---------")
     k = 10
-    num_val_samples = len(trainX) // k  # 1000 per k=5 --> FISSO!
+    num_val_samples = len(trainX) // k  # 1000 per k=5
 
     print("len(trainX)", len(trainX))
 
     print("num_val_samples", num_val_samples)
 
-    num_epochs = 100
     all_scores = []
     for i in range(k - 1):
         print('processing fold #', i)
         partial_trainX = trainX[: (i + 1) * num_val_samples]
-        # val_data = trainX[i * num_val_samples: (i + 1) * num_val_samples]
         print("partial_trainX.shape", partial_trainX.shape)
         # print("partial_train_data", partial_train_data)
 
@@ -139,7 +154,7 @@ def kFoldValidation(trainX, trainY, testX, testY):
         print("\n#########\n\n")
 
         # BUILD MODEL
-        model = buildModel2(partial_trainX, partial_trainY, valX, valY)
+        model = buildModel(partial_trainX, partial_trainY, valX, valY)
         #model.fit(partial_train_data, partial_train_targets, epochs=num_epochs, validation_data=(val_data, val_targets),
         #          batch_size=64, verbose=1)
         testLoss, testAccuracy = model.evaluate(valX, valY, batch_size=128, verbose=1)
@@ -150,98 +165,67 @@ def kFoldValidation(trainX, trainY, testX, testY):
 
 
 def scale(datasetX, datasetY, scale_type):
+
     if scale_type == "STANDARD":
-
-        scaler = StandardScaler()
-
-        # datasetX = scaler.fit_transform(datasetX)
-        scalers = {}
-        for i in range(datasetX.shape[1]):
-            scalers[i] = StandardScaler()
-            datasetX[:, i, :] = scalers[i].fit_transform(datasetX[:, i, :])
-        print("DIM TRAIN DOPO SCALER ", datasetX.shape)
-
-        ''' 
-        datasetY = scaler.fit_transform(datasetY)
-
-        datasetX = scaler.fit_transform(datasetX.reshape(-1, datasetX.shape[-1])).reshape(datasetX.shape)
-        datasetY = scaler.transform(datasetY.reshape(-1, datasetY.shape[-1])).reshape(datasetY.shape)
-
         scalers = {}
         for i in range(datasetX.shape[1]):
             scalers[i] = StandardScaler()
             datasetX[:, i, :] = scalers[i].fit_transform(datasetX[:, i, :])
 
-        '''
+        scalers2 = {}
+        for i in range(datasetY.shape[1]):
+            scalers2[i] = StandardScaler()
+            datasetY[i:] = scalers2[i].fit_transform(datasetY[i:])
+
 
     if scale_type == "MINMAX":
-        # ToDo cambiare range
-        scaler = MinMaxScaler(feature_range=(-1, 1))
-        datasetX = scaler.fit_transform(datasetX[0])
+        scalers = {}
+        for i in range(datasetX.shape[1]):
+            scalers[i] = MinMaxScaler(feature_range=(-1, 1))
+            datasetX[:, i, :] = scalers[i].fit_transform(datasetX[:, i, :])
+
+        scalers2 = {}
+        for i in range(datasetY.shape[1]):
+            scalers2[i] = MinMaxScaler(feature_range=(-1, 1))
+            datasetY[i:] = scalers2[i].fit_transform(datasetY[i:])
+
 
     if scale_type == "MAX_ABS":
-        scaler = MaxAbsScaler()
-        datasetX = scaler.fit_transform(datasetX[0])
+        scalers = {}
+        for i in range(datasetX.shape[1]):
+            scalers[i] =MaxAbsScaler()
+            datasetX[:, i, :] = scalers[i].fit_transform(datasetX[:, i, :])
+
+        scalers2 = {}
+        for i in range(datasetY.shape[1]):
+            scalers2[i] =MaxAbsScaler()
+            datasetY[i:] = scalers2[i].fit_transform(datasetY[i:])
 
     if scale_type == "ROBUST":
-        # ToDo cambiare range
-        scaler = RobustScaler(quantile_range=(25, 75), with_centering=False)
-        datasetX = scaler.fit_transform(datasetX[0])
+        scalers = {}
+        for i in range(datasetX.shape[1]):
+            scalers[i] =RobustScaler(quantile_range=(25, 75), with_centering=False)
+            datasetX[:, i, :] = scalers[i].fit_transform(datasetX[:, i, :])
+
+        scalers2 = {}
+        for i in range(datasetY.shape[1]):
+            scalers2[i] = RobustScaler(quantile_range=(25, 75), with_centering=False)
+            datasetY[i:] = scalers2[i].fit_transform(datasetY[i:])
 
     return datasetX, datasetY
 
 
-def buildModel(trainX):
-    model = models.Sequential()
-    # model.add(layers.LSTM(32))
-    model.add(layers.Dense(315, activation='relu', input_shape=(trainX.shape[1],),
-                           kernel_regularizer=tf.keras.regularizers.l2(1.e-4)))
-
-    '''
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(46, activation='relu'))  #PRIMA ERA 64
-    model.add(layers.Dense(9, activation='softmax'))
-    model.compile(optimizer='rmsprop',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    '''
-    model.add(tf.keras.layers.Dense(256, activation='relu'))
-
-    model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Dense(64, activation='relu'))
-    model.add(tf.keras.layers.Dropout(.3))
-
-    model.add(tf.keras.layers.Dense(32, activation='relu'))
-    model.add(tf.keras.layers.Dense(32, activation='relu'))
-    model.add(tf.keras.layers.Dense(9, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2(1.e-4)))
-
-    opt = tf.keras.optimizers.Adam(learning_rate=1.e-3)
-    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-
-    # model.compile(optimizer='rmsprop', loss='categorical_crossentropy',metrics=['accuracy'])
-    return model
 
 
-def buildModel2(datasetX, datasetY, testX, testY):
-    verbose, epochs, batch_size = 1, 50, 128  # prima 64
+
+def buildModel(datasetX, datasetY, testX, testY):
+    verbose, epochs, batch_size = 1, 50, 64  # prima 64
 
     n_timesteps, n_features, n_outputs = datasetX.shape[1], datasetX.shape[2], datasetY.shape[1]
     print("n_timesteps == ", n_timesteps)  # 315
     print("n_features == ", n_features)  # 3
     print("n_outputs == ", n_outputs)  # 8
-    model = Sequential()
-    # model.add(layers.Dense(200, activation='relu', input_shape=(n_timesteps,), kernel_regularizer = tf.keras.regularizers.l2(1.e-4)))
 
-    '''
-    model.add(LSTM(315, input_shape=(n_timesteps, n_features)))
-    #model.add(Dropout(0.2))
-    model.add(Dense(100, activation='relu'))
-
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(n_outputs, activation='softmax'))
-    '''
 
     # define model
     model = Sequential()
@@ -252,7 +236,6 @@ def buildModel2(datasetX, datasetY, testX, testY):
     model.add(MaxPooling1D(pool_size=4))
     model.add(Conv1D(filters=64, kernel_size=5, activation='relu'))
     model.add(MaxPooling1D(pool_size=4))
-    # model.add(LeakyReLU())
 
     model.add(Flatten())
     model.add(Dense(400, activation='relu'))
@@ -277,17 +260,20 @@ def buildModel2(datasetX, datasetY, testX, testY):
     '''
     return model
 
-
+#retrain del modello su 80% del training set completo
 def finalTraining(trainX, trainY, testX, testY):
-    model = buildModel2(trainX, trainY, testX, testY)
-    model.fit(trainX, trainY, epochs=50, batch_size=128, verbose=1)
+    model = buildModel(trainX, trainY, testX, testY)
+    model.fit(trainX, trainY, epochs=50, batch_size=64, verbose=1)
     testLoss, testAccuracy = model.evaluate(testX, testY)
 
     print("\n\n ------- accuracy su TEST: ", testAccuracy)
 
 
 def main():
+
+    #caricamento dei files e conversione da csv in txt
     trainX, trainY = loadData()
+
     '''
 
     print("shape == ", trainX.shape)
@@ -296,12 +282,13 @@ def main():
     print("shape == ", trainY.shape)
     print(trainY)
     '''
-    # print("PROVA SIMOOOO " , trainX[0].shape)
 
+    #scaling
     trainX, trainY = scale(trainX, trainY, scale_type)
 
-    # split training / test --> 80/20
+    # split training/test (80/20)
     trainX, testX = train_test_split(trainX, test_size=.2, shuffle=False)
+    trainY, testY = train_test_split(trainY, test_size=.2, shuffle=False)
 
     '''
     print("shape TRAIN == ", trainX.shape)
@@ -310,9 +297,9 @@ def main():
     print(testX)
     '''
 
-    print(" \n=============================================\n")
+    #print(" \n=============================================\n")
 
-    trainY, testY = train_test_split(trainY, test_size=.2, shuffle=False)
+
     ''' 
     print("shape TRAIN == ", trainY.shape)
     print(trainY)
@@ -326,33 +313,24 @@ def main():
 
     '''
 
+    #one-hot encoding
     trainY = tf.keras.utils.to_categorical(trainY, 8)
     testY = tf.keras.utils.to_categorical(testY, 8)
 
-    # model = buildModel(trainX)
-
     #model = buildModel2(trainX, trainY, testX, testY)
 
+    #cross validation
     kFoldValidation(trainX, trainY , testX, testY)
-    # print(getNaCount(trainX))
 
+    #addestramento e valutazione del modello su training set(80%) e test set(20%)
     finalTraining(trainX, trainY , testX, testY)
 
-    # todo ora fare ONE-HOT ENCODING
+    #todo RETRAIN FINALE SU TUTTO TRAINING!!
+    #todo FUNZIONE PER TESTSET DI INPUT
 
-    '''
-    1 split train test
-    2 prep (na, outlier (?), scaling -> standardizzare i dati, one hot)
-    3 validation (regolarizzaz si fa dentro all'add dei layers)
-    4 model fit 
-    5 data agumentation
-
-
-    '''
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
